b1c386403a4edaaf7f04da5469eae2749d8d18c0
Ohwaaan <owenlian88@gmail.com>
2023-07-30 02:14:09 -0400

    damn this hard as heck

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -6,13 +6,36 @@ loop thru arr
 return arr
 */
 
+/*
 let newArr = arg[0];
-
-for(let i = 0; i<newArr.length;i++)
+for(let i = 0; i<=newArr.length;i++)
 {
-    console.log(i);
+    for (let j = 1; j<arg.length; j++)
+    {
+        console.log('first loop :' + newArr)
+        if(newArr[i] === arg[j])
+            {
+                console.log('arra before:' + newArr)
+                newArr.splice(i,1);
+                console.log('arr arfter: '+newArr)
+            }
+    }
+
 }
-return arg[0];
+ return newArr;
+*/
+
+/*
+make new arr
+loop arr
+    loop thru notAllowed
+        if arr[i] === notAllowed
+            continue
+        newArr .push(arr[i])
+
+return newArr
+*/
+
 };
 
 // Do not edit below this line
