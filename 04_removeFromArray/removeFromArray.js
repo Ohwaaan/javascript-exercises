393b6a8be7777b84eb26aadfabef831dca4ae1cc
Owen Lian <owenlian88@gmail.com>
2023-07-29 21:43:15 -0400

    did 1 to 3 and working on 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(...arg) {
+/*
+loop thru arr
+    if element === any of nums
+        remove
+return arr
+*/
 
+let newArr = arg[0];
+
+for(let i = 0; i<newArr.length;i++)
+{
+    console.log(i);
+}
+return arg[0];
 };
 
 // Do not edit below this line
