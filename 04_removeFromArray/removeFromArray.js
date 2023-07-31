9740e53494e67b896c86ba5d3c205f374d9653fd
Owen Lian <owenlian88@gmail.com>
2023-07-30 21:35:49 -0400

    finished 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,41 +1,18 @@
 const removeFromArray = function(...arg) {
-/*
-loop thru arr
-    if element === any of nums
-        remove
-return arr
-*/
 
-/*
-let newArr = arg[0];
-for(let i = 0; i<=newArr.length;i++)
-{
-    for (let j = 1; j<arg.length; j++)
+    let newArr = [];
+    for (let i = 0; i<arg[0].length; i++)
     {
-        console.log('first loop :' + newArr)
-        if(newArr[i] === arg[j])
-            {
-                console.log('arra before:' + newArr)
-                newArr.splice(i,1);
-                console.log('arr arfter: '+newArr)
-            }
+        let isIn = false;
+        for(let j = 1; j<arg.length;j++) {
+            if (arg[0][i] === arg[j])
+                isIn = true;
+        }
+        if (!isIn) {
+            newArr.push(arg[0][i])
+        }
     }
-
-}
- return newArr;
-*/
-
-/*
-make new arr
-loop arr
-    loop thru notAllowed
-        if arr[i] === notAllowed
-            continue
-        newArr .push(arr[i])
-
-return newArr
-*/
-
+        return newArr;
 };
 
 // Do not edit below this line
