c1e8e234f61f48e3859d79d2ed13cf17a901366b
Ohwaaan <owenlian88@gmail.com>
2023-07-31 16:14:54 -0400

    kinda ate up ngl

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	let sum = 0;
+
+  nums.forEach((num) => {
+    sum+=num;
+  });
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(nums) {
+  	let sum = 1;
+
+    nums.forEach((num) => {
+      sum*=num;
+    });
+
+    return sum;
+};
+
+const power = function(num, degree) {
+	let sum = num;
+  for(let i = 1; i<degree;i++)
+  {
+    sum *=num;
+  }
+  return sum;
 
 };
 
-const power = function() {
-	
-};
+const factorial = function(num) {
+	let sum =1;
+  for(let i = 1;i<num+1;i++)
+  {
+    sum *= i;
+  }
+  return sum;
 
-const factorial = function() {
-	
 };
 
 // Do not edit below this line
